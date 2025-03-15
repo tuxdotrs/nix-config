470de70dd8b77a78e0e44747336d75902e5da61a
tux <t@tux.rs>
2025-03-16 00:40:56 +0530

    feat: setup nginx for open webui

--- modules/nixos/selfhosted/open-webui.nix
+++ modules/nixos/selfhosted/open-webui.nix
@@ -1,11 +1,35 @@
-{...}: {
-  services.open-webui = {
-    enable = true;
-    openFirewall = true;
-    host = "0.0.0.0";
-    environment = {
-      ENABLE_OLLAMA_API = "True";
-      OLLAMA_BASE_URL = "http://pc:11434";
+{
+  lib,
+  config,
+  ...
+}: {
+  services = {
+    open-webui = {
+      enable = true;
+      openFirewall = true;
+      host = "0.0.0.0";
+      port = 1111;
+      environment = {
+        WEBUI_URL = "https://chat.tux.rs";
+        ENABLE_OLLAMA_API = "True";
+        OLLAMA_BASE_URL = "http://pc:11434";
+      };
+    };
+
+    nginx = {
+      enable = lib.mkForce true;
+      virtualHosts = {
+        "chat.tux.rs" = {
+          forceSSL = true;
+          useACMEHost = "tux.rs";
+          locations = {
+            "/" = {
+              proxyPass = "http://${config.services.open-webui.host}:${toString config.services.open-webui.port}";
+              proxyWebsockets = true;
+            };
+          };
+        };
+      };
     };
   };
 }
