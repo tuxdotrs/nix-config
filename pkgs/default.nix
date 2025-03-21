dc5f0002f98eea78b4186bd820fe96d95f42c10c
tux <t@tux.rs>
2025-03-21 19:48:57 +0530

    feat: remove astronvim pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 {pkgs, ...}: {
-  astronvim = pkgs.callPackage ./astronvim {};
   firefox-mod-blur = pkgs.callPackage ./firefox-mod-blur {};
   plymouth-spinner-monochrome = pkgs.callPackage ./plymouth-spinner-monochrome {};
   go-wol = pkgs.callPackage ./go-wol {};
