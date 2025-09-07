a59383ff913c48853aad48aa8318b6112c44a7b6
tux <t@tux.rs>
2025-09-07 19:57:40 +0530

    refactor(ly): persist ly settings

--- hosts/canopus/default.nix
+++ hosts/canopus/default.nix
@@ -378,6 +378,7 @@
     ];
     files = [
       # "/etc/machine-id"
+      "/etc/ly/save.ini"
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
       "/etc/ssh/ssh_host_rsa_key"
