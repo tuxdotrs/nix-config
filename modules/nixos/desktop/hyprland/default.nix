9eb052ff02fd4d1ea55af86e5ea2b88d0d264371
tux <t@tux.rs>
2025-09-16 22:36:03 +0530

    feat: add shell aliases for monitor

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -4,4 +4,17 @@
     package = pkgs.hyprland-git.hyprland;
     portalPackage = pkgs.hyprland-git.xdg-desktop-portal-hyprland;
   };
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "mirror-display" ''
+      hyprctl keyword monitor "HDMI-A-1,preferred,0x0,1,mirror,eDP-1" \
+        && astal -q \
+        && ${pkgs.tpanel}/bin/tpanel &
+    '')
+    (pkgs.writeShellScriptBin "extend-display" ''
+      hyprctl keyword monitor "HDMI-A-1,preferred,0x-1440,1" \
+        && astal -q \
+        && ${pkgs.tpanel}/bin/tpanel &
+    '')
+  ];
 }
