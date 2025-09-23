f89d27db2e2624b408171f46c90319f09fc718b8
tux <t@tux.rs>
2025-09-23 13:36:48 +0530

    chore: update sha256 hash

--- pkgs/firefox-mod-blur/default.nix
+++ pkgs/firefox-mod-blur/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
     owner = "datguypiko";
     repo = "Firefox-Mod-Blur";
     rev = "refs/heads/master";
-    sha256 = "sha256-YcADnfZSs4Tv99NHyvwip8sE6tXh5sQHj/SXgC/Nxc4=";
+    sha256 = "sha256-zokmzwv+JcGhuX5GMySISE7ErrUz2NL+NYB/74JYi5g=";
   };
 
   installPhase = ''
