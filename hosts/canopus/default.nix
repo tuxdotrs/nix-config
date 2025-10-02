4dbd7264a2e38c582ee49ec2f336a822e7a6c81b
tux <t@tux.rs>
2025-10-02 13:54:25 +0530

    feat: update asusd config

--- hosts/canopus/default.nix
+++ hosts/canopus/default.nix
@@ -291,25 +291,30 @@
       asusdConfig.text = ''
         (
           charge_control_end_threshold: 80,
-          panel_od: false,
-          mini_led_mode: false,
           disable_nvidia_powerd_on_battery: true,
           ac_command: "",
           bat_command: "",
-          platform_policy_on_battery: Quiet,
-          platform_policy_on_ac: Quiet,
-          ppt_pl1_spl: None,
-          ppt_pl2_sppt: None,
-          ppt_fppt: None,
-          ppt_apu_sppt: None,
-          ppt_platform_sppt: None,
-          nv_dynamic_boost: None,
-          nv_temp_target: None,
+
+          platform_profile_linked_epp: true,
+          platform_profile_on_battery: Quiet,
+          platform_profile_on_ac: Performance,
+
+          change_platform_profile_on_battery: true,
+          change_platform_profile_on_ac: true,
+
+          profile_quiet_epp: Power,
+          profile_balanced_epp: BalancePower,
+          profile_custom_epp: Performance,
+          profile_performance_epp: Performance,
+
+          ac_profile_tunings: {},
+          dc_profile_tunings: {},
+          armoury_settings: {},
         )
       '';
       profileConfig.text = ''
         (
-          active_profile: Balanced,
+          active_profile: Quiet,
         )
       '';
       fanCurvesConfig.text = ''
@@ -320,13 +325,13 @@
                 fan: CPU,
                 pwm: (2, 22, 45, 68, 91, 153, 153, 153),
                 temp: (55, 62, 66, 70, 74, 78, 78, 78),
-                enabled: true,
+                enabled: false,
               ),
               (
                 fan: GPU,
-                pwm: (2, 25, 48, 71, 94, 165, 165, 165)
+                pwm: (2, 25, 48, 71, 94, 165, 165, 165),
                 temp: (55, 62, 66, 70, 74, 78, 78, 78),
-                enabled: true,
+                enabled: false,
               ),
             ],
             performance: [
@@ -334,19 +339,19 @@
                 fan: CPU,
                 pwm: (35, 68, 79, 91, 114, 175, 175, 175),
                 temp: (58, 62, 66, 70, 74, 78, 78, 78),
-                enabled: true,
+                enabled: false,
               ),
               (
                 fan: GPU,
                 pwm: (35, 71, 84, 94, 119, 188, 188, 188),
                 temp: (58, 62, 66, 70, 74, 78, 78, 78),
-                enabled: true,
+                enabled: false,
               ),
             ],
             quiet: [
               (
                 fan: CPU,
-                pwm: (2, 12, 25, 35, 48, 61, 84, 90),
+                pwm: (2, 12, 22, 35, 45, 58, 79, 79),
                 temp: (55, 62, 66, 70, 74, 78, 82, 82),
                 enabled: true,
               ),
