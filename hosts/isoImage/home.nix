c8e02ba8477c26a95de926f4bfb242b81450a93c
tux <t@tux.rs>
2025-10-11 23:45:49 +0530

    refactor: remove unused modules

--- hosts/isoImage/home.nix
+++ hosts/isoImage/home.nix
@@ -3,16 +3,10 @@
     ../../modules/home/desktop/awesome
     ../../modules/home/desktop/hyprland
     ../../modules/home/picom
-    ../../modules/home/alacritty
     ../../modules/home/wezterm
-    ../../modules/home/ghostty
     ../../modules/home/desktop/rofi
-    ../../modules/home/barrier
     ../../modules/home/firefox
     ../../modules/home/brave
-    ../../modules/home/vs-code
-    ../../modules/home/mopidy
-    ../../modules/home/thunderbird
   ];
 
   home.pointerCursor = {
