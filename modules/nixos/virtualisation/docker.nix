a793ff14c4a58868b964597c38a5a85aadd82205
tux <t@tux.rs>
2025-10-15 18:28:21 +0530

    feat(docker): add nvidia-container-toolkit support

--- modules/nixos/virtualisation/docker.nix
+++ modules/nixos/virtualisation/docker.nix
@@ -8,6 +8,8 @@
     docker.enable = true;
   };
 
+  hardware.nvidia-container-toolkit.enable = true;
+
   environment.systemPackages = with pkgs; [lazydocker];
 
   users.users.${username}.extraGroups = ["docker"];
