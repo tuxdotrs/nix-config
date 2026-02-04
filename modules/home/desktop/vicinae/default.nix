3a46142f6fed8c71e688224a98015ca5504062ff
tux <t@tux.rs>
2026-02-04 06:31:45 +0530

    feat(vicinae): disable close on focus loss

--- modules/home/desktop/vicinae/default.nix
+++ modules/home/desktop/vicinae/default.nix
@@ -8,7 +8,7 @@
     useLayerShell = true;
 
     settings = {
-      close_on_focus_loss = true;
+      close_on_focus_loss = false;
       consider_preedit = true;
       pop_to_root_on_close = true;
       favicon_service = "twenty";
