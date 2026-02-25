6403fab5af751e9e0330befea0baec721b8b04e3
tux <t@tux.rs>
2026-02-25 13:24:36 +0530

    feat(obs): add source record plugin

--- modules/nixos/obs-studio.nix
+++ modules/nixos/obs-studio.nix
@@ -2,6 +2,6 @@
   programs.obs-studio = {
     enable = true;
     enableVirtualCamera = true;
-    plugins = with pkgs.obs-studio-plugins; [obs-vaapi wlrobs];
+    plugins = with pkgs.obs-studio-plugins; [obs-vaapi wlrobs obs-source-record];
   };
 }
