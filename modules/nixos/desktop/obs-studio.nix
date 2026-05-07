376f5d053edb11a236a89dc7e3513a6382130277
tux <t@tux.rs>
2026-05-07 19:19:08 +0530

    feat: setup obs-studi module

---
+++ modules/nixos/desktop/obs-studio.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.nixos.desktop =
+    { pkgs, ... }:
+    {
+      programs.obs-studio = {
+        enable = true;
+        enableVirtualCamera = true;
+        plugins = with pkgs.obs-studio-plugins; [
+          obs-vaapi
+          wlrobs
+          obs-source-record
+        ];
+      };
+    };
+}
