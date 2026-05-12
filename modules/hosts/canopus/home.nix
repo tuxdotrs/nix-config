fdc63b93078ccc3d2df61e0705a774ce6963dfb6
tux <t@tux.rs>
2026-05-12 21:01:53 +0530

    refactor(desktop): standardize mangowm settings across hosts

--- modules/hosts/canopus/home.nix
+++ modules/hosts/canopus/home.nix
@@ -5,12 +5,31 @@
       desktop
     ];
 
-    tnix.services.lan-mouse = {
-      enable = true;
-      settings = {
-        authorized_fingerprints = {
-          "f4:4b:17:61:f7:01:a4:a2:e1:c7:8c:1c:7a:f3:8b:87:14:3d:05:3d:a0:8b:cc:e7:88:d8:d8:d2:a4:c2:75:8b" =
-            "sirius";
+    tnix = {
+      desktop = {
+        mangowm = {
+          enable = true;
+          monitorRule = [
+            "name:eDP-1, width:2560, height:1440, refresh:165, x:0, y:0, vrr:1"
+          ];
+
+          tagRule = [
+            "id:1, layout_name:tile"
+            "id:2, layout_name:tile"
+            "id:3, layout_name:tile"
+            "id:4, layout_name:tile"
+            "id:5, layout_name:scroller"
+          ];
+        };
+      };
+
+      services.lan-mouse = {
+        enable = true;
+        settings = {
+          authorized_fingerprints = {
+            "f4:4b:17:61:f7:01:a4:a2:e1:c7:8c:1c:7a:f3:8b:87:14:3d:05:3d:a0:8b:cc:e7:88:d8:d8:d2:a4:c2:75:8b" =
+              "sirius";
+          };
         };
       };
     };
--- modules/hosts/sirius/home.nix
+++ modules/hosts/sirius/home.nix
@@ -5,17 +5,50 @@
       desktop
     ];
 
-    tnix.services.lan-mouse = {
-      enable = true;
-      settings = {
-        clients = [
-          {
-            position = "bottom";
-            hostname = "canopus";
-            activate_on_startup = true;
-            ips = [ "192.168.8.2" ];
-          }
-        ];
+    tnix = {
+      desktop = {
+        mangowm = {
+          enable = true;
+          monitorRule = [
+            "name:DP-2, width:1440, height:2560, refresh:144, x:0, y:0, vrr:0, rr:1"
+            "name:DP-3, width:2560, height:1440, refresh:144, x:1440, y:0, vrr:0"
+            "name:DP-1, width:1080, height:1920, refresh:144, x:4000, y:0, vrr:0, rr:3"
+          ];
+
+          tagRule = [
+            "id:1, layout_name:vertical_tile, monitor_name:DP-2, no_hide:1"
+            "id:2, layout_name:vertical_tile, monitor_name:DP-2, no_hide:1"
+            "id:3, layout_name:vertical_tile, monitor_name:DP-2, no_hide:1"
+            "id:4, layout_name:vertical_tile, monitor_name:DP-2, no_hide:1"
+            "id:5, layout_name:vertical_tile, monitor_name:DP-2, no_hide:1"
+
+            "id:1, layout_name:tile, monitor_name:DP-3, no_hide:1"
+            "id:2, layout_name:tile, monitor_name:DP-3, no_hide:1"
+            "id:3, layout_name:tile, monitor_name:DP-3, no_hide:1"
+            "id:4, layout_name:tile, monitor_name:DP-3, no_hide:1"
+            "id:5, layout_name:tile, monitor_name:DP-3, no_hide:1"
+
+            "id:1, layout_name:vertical_tile, monitor_name:DP-1, no_hide:1"
+            "id:2, layout_name:vertical_tile, monitor_name:DP-1, no_hide:1"
+            "id:3, layout_name:vertical_tile, monitor_name:DP-1, no_hide:1"
+            "id:4, layout_name:vertical_tile, monitor_name:DP-1, no_hide:1"
+            "id:5, layout_name:vertical_tile, monitor_name:DP-1, no_hide:1"
+          ];
+        };
+      };
+
+      services.lan-mouse = {
+        enable = true;
+        settings = {
+          clients = [
+            {
+              position = "bottom";
+              hostname = "canopus";
+              activate_on_startup = true;
+              ips = [ "192.168.8.2" ];
+            }
+          ];
+        };
       };
     };
 
