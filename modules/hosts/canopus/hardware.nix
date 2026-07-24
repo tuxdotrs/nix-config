a63df0ee62c38273e15f66716eb7965126bd3e9a
tux <t@tux.rs>
2026-07-24 17:54:50 +0530

    feat(canopus): enable dGPU

--- modules/hosts/canopus/hardware.nix
+++ modules/hosts/canopus/hardware.nix
@@ -35,7 +35,7 @@
         supergfxd = {
           enable = true;
           settings = {
-            mode = "Integrated";
+            mode = "Hybrid";
             vfio_enable = false;
             vfio_save = false;
             always_reboot = false;
