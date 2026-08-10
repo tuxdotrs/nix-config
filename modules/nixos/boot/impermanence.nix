cf52a84c81b67315877f159735b48b1af0bafdfc
tux <t@tux.rs>
2026-08-10 20:44:46 +0530

    fix(boot): ensure machine-id is persisted

--- modules/nixos/boot/impermanence.nix
+++ modules/nixos/boot/impermanence.nix
@@ -54,6 +54,7 @@
           ++ cfg.impermanence.directories;
 
           files = [
+            "/etc/machine-id"
             "/etc/ssh/ssh_host_ed25519_key"
             "/etc/ssh/ssh_host_ed25519_key.pub"
             "/etc/ssh/ssh_host_rsa_key"
