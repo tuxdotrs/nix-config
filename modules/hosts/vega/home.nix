f2d133b58a5f42ecf3f0c1dd367d66cccd5e73f7
tux <t@tux.rs>
2026-09-20 05:51:36 +0530

    refactor(hosts): clean up vega home configuration

--- modules/hosts/vega/home.nix
+++ modules/hosts/vega/home.nix
@@ -1,8 +1,5 @@
-{lib, ...}: {
+{
   flake.modules.homeManager.vega = {
-    # @TODO: Broken currently - By default it's enabled by neovim module
-    programs.vim.enable = lib.mkForce false;
-
     home.stateVersion = "26.05";
   };
 }
